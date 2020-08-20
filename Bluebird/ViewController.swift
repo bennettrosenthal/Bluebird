018d8dd1a7e06879dd15acc8a99a0057bee84dbb
TheModernEra <bsrosenthal101@gmail.com>
2020-08-20 16:39:00 -0700

    added uninstall button concept, fixed up UI

--- Bluebird/ViewController.swift
+++ Bluebird/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import Cocoa
+import Foundation
+import Alamofire
+import SSZipArchive
 
 class ViewController: NSViewController {
 
@@ -22,15 +25,32 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBOutlet weak var selectionLabel: NSTextField!
     @IBOutlet weak var gameSelectionDropdown: NSPopUpButtonCell!
     @IBOutlet weak var statusLabel: NSTextField!
+    @IBOutlet weak var uninstallButton: NSButton!
     
+    @IBAction func gameSelectionDropdownChanged(_ sender: Any) {
+        var gameChosen = String()
+        gameChosen = gameSelectionDropdown.titleOfSelectedItem!
+        selectionLabel.stringValue = ("What do you want to do with " + gameChosen + "?")
+    }
     
     @IBAction func goButtonPressed(_ sender: Any) {
         var gameChosen = String()
         gameChosen = gameSelectionDropdown.titleOfSelectedItem!
         statusLabel.stringValue = ("Downloading " + gameChosen + "...")
     }
-    
+    var amount = 0
+    @IBAction func uninstallButtonPressed(_ sender: Any) {
+        amount += 1
+        if amount == 1 {
+            uninstallButton.title = "Are you sure?"
+        }
+        print(amount)
+        if amount == 2 {
+            uninstallButton.title = "Uninstalling..."
+        }
+    }
 }
 
