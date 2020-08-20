c051b294e647c15585714ba5f46922c7d444015b
TheModernEra <bsrosenthal101@gmail.com>
2020-08-20 15:31:10 -0700

    Initial Commit

---
+++ Bluebird/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bluebird
+//
+//  Created by Bennett Rosenthal on 8/20/20.
+//  Copyright © 2020 ModernEra. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Bluebird/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bluebird
+//
+//  Created by Bennett Rosenthal on 8/20/20.
+//  Copyright © 2020 ModernEra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
