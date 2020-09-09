ef8d509ae1936bad1c571b5970507f7af8b931f6
ModernEra <bsrosenthal101@gmail.com>
2020-09-09 00:15:25 -0700

    my two remaining brain cells figured out how to get the info loaded after the txt is downloaded

--- Bluebird/AppDelegate.swift
+++ Bluebird/AppDelegate.swift
@@ -15,21 +15,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        let destination: DownloadRequest.Destination = { _, _ in
-        let documentsURL = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask)[0]
-        let fileURL = documentsURL.appendingPathComponent("upsiopts.txt")
-
-        return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
-        }
-
-        AF.download("https://thesideloader.co.uk/upsiopts.txt", to: destination).response { response in
-        debugPrint(response)
-
-            if response.error == nil, let imagePath = response.fileURL?.path {
-            
             }
-        }
-    }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
--- Bluebird/ViewController.swift
+++ Bluebird/ViewController.swift
@@ -35,6 +35,119 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let destination: DownloadRequest.Destination = { _, _ in
+        let documentsURL = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask)[0]
+        let fileURL = documentsURL.appendingPathComponent("upsiopts.txt")
+
+        return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
+        }
+
+        AF.download("https://thesideloader.co.uk/upsiopts.txt", to: destination).response { response in
+        debugPrint(response)
+
+            if response.error == nil, let imagePath = response.fileURL?.path {
+                do {
+                    // defines array from txt file
+                    let txtPath: String = "\(self.usernameFilePath)/Downloads/upsiopts.txt"
+                    let txtFile = try String(contentsOfFile: txtPath)
+                    let txtArray: [String] = txtFile.components(separatedBy: "\n")
+                    let separator = "END\r"
+                    let array = txtArray.split(separator: separator)
+                    
+                    // creates Contractors Array
+                    let contractorsArray = Array(array[0])
+                    // gets necessary variables for contractors
+                    
+                    // gets Contractors URL
+                    let firstContractURL = contractorsArray.firstIndex(where: {$0.hasPrefix("DOWNLOADFROM=") })!
+                    let secondContractURL = contractorsArray[firstContractURL]
+                    let thirdContractURL = secondContractURL.replacingOccurrences(of: "DOWNLOADFROM=", with: "")
+                    self.contractorsURL = thirdContractURL.replacingOccurrences(of: "\r", with: "")
+                    print(self.contractorsURL)
+                    
+                    // gets Contractors OBB file name
+                    let firstContractOBB = contractorsArray.firstIndex(where: {$0.hasPrefix("OBB=") })!
+                    let secondContractOBB = contractorsArray[firstContractOBB]
+                    let thirdContractOBB = secondContractOBB.replacingOccurrences(of: "OBB=", with: "")
+                    self.contractorsOBBName = thirdContractOBB.replacingOccurrences(of: "\r", with: "")
+                    print(self.contractorsOBBName)
+                    
+                    // gets Contractors APK file name
+                    let firstContractAPK = contractorsArray.firstIndex(where: {$0.hasPrefix("APK=") })!
+                    let secondContractAPK = contractorsArray[firstContractAPK]
+                    let thirdContractAPK = secondContractAPK.replacingOccurrences(of: "APK=", with: "")
+                    self.contractorsAPKName = thirdContractAPK.replacingOccurrences(of: "\r", with: "")
+                    print(self.contractorsAPKName)
+                    
+                    // gets Contractors folder name
+                    let firstContractName = contractorsArray.firstIndex(where: {$0.hasPrefix("ZIPNAME=") })!
+                    let secondContractName = contractorsArray[firstContractName]
+                    let thirdContractName = secondContractName.replacingOccurrences(of: "ZIPNAME=", with: "")
+                    let fourthContractName = thirdContractName.replacingOccurrences(of: ".zip", with: "")
+                    self.contractorsBuildName = fourthContractName.replacingOccurrences(of: "\r", with: "")
+                    print(self.contractorsBuildName)
+                    
+                    // creates Pavlov Array
+                    let pavlovArray = Array(array[1])
+                    // gets necessary variables for pavlov
+                    
+                    // URL for pavlov
+                    let firstPavlovURL = pavlovArray.firstIndex(where: { $0.hasPrefix("DOWNLOADFROM=") })!
+                    let secondPavlovURL = pavlovArray[firstPavlovURL]
+                    let thirdPavlovURL = secondPavlovURL.replacingOccurrences(of: "DOWNLOADFROM=", with: "")
+                    self.pavlovURL = thirdPavlovURL.replacingOccurrences(of: "\r", with: "")
+                    
+                    // OBB file name
+                    let firstOBBName = pavlovArray.firstIndex(where: {$0.hasPrefix("OBB=")})!
+                    let secondOBBName = pavlovArray[firstOBBName]
+                    let thirdOBBName = secondOBBName.replacingOccurrences(of: "OBB=", with: "")
+                    self.pavlovOBBName = thirdOBBName.replacingOccurrences(of: "\r", with: "")
+                    
+                    // current build name
+                    let firstBuildName = pavlovArray.firstIndex(where: {$0.hasPrefix("ZIPNAME=")})!
+                    let secondBuildName = pavlovArray[firstBuildName]
+                    let thirdBuildName = secondBuildName.replacingOccurrences(of: "ZIPNAME=", with: "")
+                    let fourthBuildName = thirdBuildName.replacingOccurrences(of: ".zip", with: "")
+                    self.pavlovBuildName = fourthBuildName.replacingOccurrences(of: "\r", with: "")
+                    
+                    // APK file name
+                    let firstAPKName = pavlovArray.firstIndex(where: {$0.hasPrefix("APK=")})!
+                    let secondAPKName = pavlovArray[firstAPKName]
+                    let thirdAPKName = secondAPKName.replacingOccurrences(of: "APK=", with: "")
+                    self.pavlovAPKName = thirdAPKName.replacingOccurrences(of: "\r", with: "")
+                    
+                    // creates Hi-Bow Array
+                    let hibowArray = Array(array[2])
+                    // get necessary variables for hi-bow
+                    
+                    // get hi-bow URL
+                    let firstBowURL = hibowArray.firstIndex(where: {$0.hasPrefix("DOWNLOADFROM=") })!
+                    let secondBowURL = hibowArray[firstBowURL]
+                    let thirdBowURL = secondBowURL.replacingOccurrences(of: "DOWNLOADFROM=", with: "")
+                    self.hibowURL = thirdBowURL.replacingOccurrences(of: "\r", with: "")
+                    print(self.hibowURL)
+                                
+                    // get hi-bow APK name
+                    let firstBowAPK = hibowArray.firstIndex(where: {$0.hasPrefix("APK=") })!
+                    let secondBowAPK = hibowArray[firstBowAPK]
+                    let thirdBowAPK = secondBowAPK.replacingOccurrences(of: "APK=", with: "")
+                    self.hibowAPKName = thirdBowAPK.replacingOccurrences(of: "\r", with: "")
+                    print(self.hibowAPKName)
+                    
+                    // get hi-bow folder name
+                    let firstBowFolder = hibowArray.firstIndex(where: {$0.hasPrefix("ZIPNAME=") })!
+                    let secondBowFolder = hibowArray[firstBowFolder]
+                    let thirdBowFolder = secondBowFolder.replacingOccurrences(of: "ZIPNAME=", with: "")
+                    let fourthBowFolder = thirdBowFolder.replacingOccurrences(of: ".zip", with: "")
+                    self.hibowBuildName = fourthBowFolder.replacingOccurrences(of: "\r", with: "")
+                    print(self.hibowBuildName)
+                }
+                catch {
+                    print(error)
+                }
+            }
+        }
+
         self.downloadProgressIndicator.isHidden = true
     }
 
@@ -56,106 +169,6 @@ class ViewController: NSViewController {
     }
     
     @IBAction func goButtonPressed(_ sender: Any) {
-        do {
-            // defines array from txt file
-            let txtPath: String = "\(self.usernameFilePath)/Downloads/upsiopts.txt"
-            let txtFile = try String(contentsOfFile: txtPath)
-            let txtArray: [String] = txtFile.components(separatedBy: "\n")
-            let separator = "END\r"
-            let array = txtArray.split(separator: separator)
-            
-            // creates Contractors Array
-            let contractorsArray = Array(array[0])
-            // gets necessary variables for contractors
-            
-            // gets Contractors URL
-            let firstContractURL = contractorsArray.firstIndex(where: {$0.hasPrefix("DOWNLOADFROM=") })!
-            let secondContractURL = contractorsArray[firstContractURL]
-            let thirdContractURL = secondContractURL.replacingOccurrences(of: "DOWNLOADFROM=", with: "")
-            contractorsURL = thirdContractURL.replacingOccurrences(of: "\r", with: "")
-            print(contractorsURL)
-            
-            // gets Contractors OBB file name
-            let firstContractOBB = contractorsArray.firstIndex(where: {$0.hasPrefix("OBB=") })!
-            let secondContractOBB = contractorsArray[firstContractOBB]
-            let thirdContractOBB = secondContractOBB.replacingOccurrences(of: "OBB=", with: "")
-            contractorsOBBName = thirdContractOBB.replacingOccurrences(of: "\r", with: "")
-            print(contractorsOBBName)
-            
-            // gets Contractors APK file name
-            let firstContractAPK = contractorsArray.firstIndex(where: {$0.hasPrefix("APK=") })!
-            let secondContractAPK = contractorsArray[firstContractAPK]
-            let thirdContractAPK = secondContractAPK.replacingOccurrences(of: "APK=", with: "")
-            contractorsAPKName = thirdContractAPK.replacingOccurrences(of: "\r", with: "")
-            print(contractorsAPKName)
-            
-            // gets Contractors folder name
-            let firstContractName = contractorsArray.firstIndex(where: {$0.hasPrefix("ZIPNAME=") })!
-            let secondContractName = contractorsArray[firstContractName]
-            let thirdContractName = secondContractName.replacingOccurrences(of: "ZIPNAME=", with: "")
-            let fourthContractName = thirdContractName.replacingOccurrences(of: ".zip", with: "")
-            contractorsBuildName = fourthContractName.replacingOccurrences(of: "\r", with: "")
-            print(contractorsBuildName)
-            
-            // creates Pavlov Array
-            let pavlovArray = Array(array[1])
-            // gets necessary variables for pavlov
-            
-            // URL for pavlov
-            let firstPavlovURL = pavlovArray.firstIndex(where: { $0.hasPrefix("DOWNLOADFROM=") })!
-            let secondPavlovURL = pavlovArray[firstPavlovURL]
-            let thirdPavlovURL = secondPavlovURL.replacingOccurrences(of: "DOWNLOADFROM=", with: "")
-            pavlovURL = thirdPavlovURL.replacingOccurrences(of: "\r", with: "")
-            
-            // OBB file name
-            let firstOBBName = pavlovArray.firstIndex(where: {$0.hasPrefix("OBB=")})!
-            let secondOBBName = pavlovArray[firstOBBName]
-            let thirdOBBName = secondOBBName.replacingOccurrences(of: "OBB=", with: "")
-            pavlovOBBName = thirdOBBName.replacingOccurrences(of: "\r", with: "")
-            
-            // current build name
-            let firstBuildName = pavlovArray.firstIndex(where: {$0.hasPrefix("ZIPNAME=")})!
-            let secondBuildName = pavlovArray[firstBuildName]
-            let thirdBuildName = secondBuildName.replacingOccurrences(of: "ZIPNAME=", with: "")
-            let fourthBuildName = thirdBuildName.replacingOccurrences(of: ".zip", with: "")
-            pavlovBuildName = fourthBuildName.replacingOccurrences(of: "\r", with: "")
-            
-            // APK file name
-            let firstAPKName = pavlovArray.firstIndex(where: {$0.hasPrefix("APK=")})!
-            let secondAPKName = pavlovArray[firstAPKName]
-            let thirdAPKName = secondAPKName.replacingOccurrences(of: "APK=", with: "")
-            pavlovAPKName = thirdAPKName.replacingOccurrences(of: "\r", with: "")
-            
-            // creates Hi-Bow Array
-            let hibowArray = Array(array[2])
-            // get necessary variables for hi-bow
-            
-            // get hi-bow URL
-            let firstBowURL = hibowArray.firstIndex(where: {$0.hasPrefix("DOWNLOADFROM=") })!
-            let secondBowURL = hibowArray[firstBowURL]
-            let thirdBowURL = secondBowURL.replacingOccurrences(of: "DOWNLOADFROM=", with: "")
-            hibowURL = thirdBowURL.replacingOccurrences(of: "\r", with: "")
-            print(hibowURL)
-                        
-            // get hi-bow APK name
-            let firstBowAPK = hibowArray.firstIndex(where: {$0.hasPrefix("APK=") })!
-            let secondBowAPK = hibowArray[firstBowAPK]
-            let thirdBowAPK = secondBowAPK.replacingOccurrences(of: "APK=", with: "")
-            hibowAPKName = thirdBowAPK.replacingOccurrences(of: "\r", with: "")
-            print(hibowAPKName)
-            
-            // get hi-bow folder name
-            let firstBowFolder = hibowArray.firstIndex(where: {$0.hasPrefix("ZIPNAME=") })!
-            let secondBowFolder = hibowArray[firstBowFolder]
-            let thirdBowFolder = secondBowFolder.replacingOccurrences(of: "ZIPNAME=", with: "")
-            let fourthBowFolder = thirdBowFolder.replacingOccurrences(of: ".zip", with: "")
-            hibowBuildName = fourthBowFolder.replacingOccurrences(of: "\r", with: "")
-            print(hibowBuildName)
-        }
-        catch {
-            print(error)
-        }
-        
         if gameSelected == "Contractors" {
             statusLabel.stringValue = "Downloading latest version of Contractors..."
             self.downloadProgressIndicator.isHidden = false
